61f0046ed869550925747dfca1af1f507718f238
“nfdiop” <ndeye-fatou.diop@berkeley.edu>
2025-03-16 14:53:52 +0100

    Add timeutils object

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import moment from "moment";
+import { TimeUtils } from "../../utils/time";
 
 export function Clock() {
   const [time, setTime] = useState(new Date());
@@ -14,7 +14,7 @@ export function Clock() {
   return (
     <div className="clock-container">
       <h3 className="time">
-        {moment(time).format("MMMM Do YYYY, h:mm:ss a")}.
+        {TimeUtils.getTimeInFormat(time, "MMMM Do YYYY, h:mm:ss a")}.
       </h3>
     </div>
   );
